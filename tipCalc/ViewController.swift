66202567b910a218118b78041f0a59fdd39cf1ec
Yixing Wang <wyx@yixings-macbook-pro.local>
2019-02-11 00:04:15 -0800

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Yixing Wang on 2/11/19.
+//  Copyright © 2019 yxpersonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
